d17a024d9ba34a1e5df01063df5ceeed08505c98
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-15 15:28:37 -0700

    Initial Commit

---
+++ FootballTeams/FootballTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTeamsApp.swift
+//  FootballTeams
+//
+//  Created by Lesley Lopez on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FootballTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
